e3a5de10375befa57dbdac4682468e99ea47e2aa
Grace <mgracebodur@gmail.com>
2020-04-13 19:38:26 -0400

    Added some landing page styles, still need to refactor

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,10 +4,10 @@ import './Footer.css'
 
 function Footer() {
 	return (
-		<footer className='footerText zone'>
-			<h2 className='right'>Created by:</h2>
-			<a className='footerLink author' href='https://gracebodur.github.io/devgrace-portfolio/' target='_blank' rel='noopener noreferrer'>Grace Bodur</a>
-			<a className='footerLink author' href='https://malekadair.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Malek Haj-Hussein</a>
+		<footer className='zone main'>
+			<h2 className='footer-link footer-right'>Created by:</h2>
+			<a className='footer-link' href='https://gracebodur.github.io/devgrace-portfolio/' target='_blank' rel='noopener noreferrer'>Grace Bodur</a>
+			<a className='footer-link' href='https://malekadair.github.io/portfolio/' target='_blank' rel='noopener noreferrer'>Malek Haj-Hussein</a>
 		</footer>
 	)
 }
--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -1,25 +1,30 @@
 import React, { Component } from 'react'
+import cover from '../../img/cover-2.png'
+import green_ball from '../../img/green_ball.png'
+import yellow_ball from '../../img/yellow_ball.png'
+import red_ball from '../../img/red_ball.png'
+import './Welcome.css'
 
 class Welcome extends Component {
 	render() {
 		return (
-			<div>
-				<h2>Welcome!</h2>
-				<section className='welcome'>
-					<div className='left container'>
-						<img src='https://via.placeholder.com/150' />
+			<main>
+			<div className='container zone'><img class='cover' src={cover} alt='lottery-balls'/></div>
+				<div className='zone grid-wrapper'>
+					<div className='box zone'>
+						<img src={green_ball} alt='green lottery ball' />
 						<p className='cardContent'>Veniam occaecat ullamco ut officia deserunt tempor.</p>
 					</div>
-					<div className='middle container'>
-						<img src='https://via.placeholder.com/150' />
+					<div className='box zone'>
+						<img src={yellow_ball} alt='yellow lottery ball'/>
 						<p className='cardContent'>Ex eiusmod ad laborum sunt fugiat adipisicing elit duis esse amet.</p>
 					</div>
-					<div className='right container'>
-						<img src='https://via.placeholder.com/150' />
+					<div className='box zone'>
+						<img src={red_ball} alt='lottery ball' />
 						<p className='cardContent'>Laboris sit laboris dolore do deserunt fugiat veniam dolore deserunt consectetur nisi esse velit.</p>
 					</div>
-				</section>
-			</div>
+				</div>
+			</main>
 		)
 	}
 }
